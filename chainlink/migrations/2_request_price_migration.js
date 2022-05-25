741026f2bef71c887d97e3003d8e8b56c6d1bbb9
Michael Sands <michaelesands@gmail.com>
2022-05-25 01:53:59 -0400

    chainlink w truffle via localhost

--- chainlink/migrations/2_request_price_migration.js
+++ chainlink/migrations/2_request_price_migration.js
@@ -1,5 +1,5 @@
-// const Migrations = artifacts.require("RequestPrice");
+const RequestPrice = artifacts.require("RequestPrice");
 
-// module.exports = function (deployer) {
-//   deployer.deploy(Migrations);
-// };
+module.exports = function (deployer) {
+  deployer.deploy(RequestPrice);
+};
